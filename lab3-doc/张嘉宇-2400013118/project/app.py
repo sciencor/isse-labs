1ac481eee50231a1df98d74092fb8994dc0c7748
kuiningzzzz <1396298976@qq.com>
2025-10-17 19:38:12 +0800

    (finish)front_end

--- lab3-doc/张嘉宇-2400013118/project/app.py
+++ lab3-doc/张嘉宇-2400013118/project/app.py
@@ -1,7 +1,9 @@
 from flask import Flask, jsonify, request
+from flask_cors import CORS
 
 
 app = Flask(__name__)
+CORS(app) # Enable CORS for all routes
 
 # Simple in-memory storage; replace with persistent store later.
 tasks = []
