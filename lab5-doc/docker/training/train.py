aa5a4c814e217d57d31001fec7428280a361d578
82327 <2300017850@stu.pku.edu.cn>
2025-11-13 22:52:36 +0800

    complete almost

--- lab5-doc/docker/training/train.py
+++ lab5-doc/docker/training/train.py
@@ -1,6 +1,10 @@
 from sklearn.svm import SVC
 from sklearn import datasets
 import joblib
+import os
+
+save_path = '/app/models/iris_model.pkl'
+os.makedirs(os.path.dirname(save_path), exist_ok=True)
 
 #Load the Iris dataset
 iris = datasets.load_iris()
@@ -12,7 +16,7 @@ clf = SVC()
 model = clf.fit(iris.data, iris.target_names[iris.target])
 
 # TODO: Save the trained model to the shared volume (make sure to use the correct path)
-joblib.dump(model, '...')
+joblib.dump(model, save_path)
 
 print("Model training complete and saved as iris_model.pkl")
 
