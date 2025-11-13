c4333ddf24ce679478107f452c122613a6cf6090
Tort0iseZzz <2400013107@stu.pku.edu.cn>
2025-11-13 12:24:59 +0800

    step2 server finished

--- lab5-doc/王嘉怡-2400013107/docker/inference/server.py
+++ lab5-doc/王嘉怡-2400013107/docker/inference/server.py
@@ -5,19 +5,20 @@ import joblib
 app = Flask(__name__)
 
 # TODO: Load the trained model from the shared volume (use the correct path)
-model = ...
+model = joblib.load('/app/models/iris_model.pkl')
 
 # TODO: Add request method to predict
-@app.route('/predict', methods=[''])
+@app.route('/predict', methods=['POST'])
 def predict():
     # TODO: Get the input array from the request body and make prediction using the model
     get_json = request.get_json()
-    iris_input = ...
+    iris_input = get_json['input']
 
     # HINT: use np.array().reshape(1, -1) to convert input to 2D array
-    prediction = ...
+    data_to_predict = np.array(iris_input).reshape(1, -1)
+    prediction = model.predict(data_to_predict)
 
-    return ...
+    return jsonify({'prediction': prediction[0]})
 
 @app.route('/')
 def hello():
